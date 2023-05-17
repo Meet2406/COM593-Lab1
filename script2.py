55f2c8a5fa1472fc81ed4112c236482032c6d6be
Mark <mark.noble@flemingcollege.ca>
2023-05-17 15:42:56 -0400

    Modified both files in the repo clone
    -Deleted a print()statement from script1.py
    -Added two print()statements to script2.py

--- script2.py
+++ script2.py
@@ -1,3 +1,4 @@
 # This is good programing advice
 print("Sparse is better than dense.")
 print("Although that way may not be obvious at first unless you're Dutch.")
+print("Flat is better than nested.")
